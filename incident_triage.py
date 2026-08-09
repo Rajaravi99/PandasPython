5d129b0684a8aa3e681c61703eb75f360f565cf1
Rajaravi99 <ravinandanray99@gmail.com>
2026-08-09 15:38:10 +0530

    test commit

--- incident_triage.py
+++ incident_triage.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import re
+from urllib.parse import urlparse
 from datetime import datetime
 
 df = pd.read_csv('Input_alert_data/SentinelHighSeverityIncidentsDataperweek.csv')
-print(df.head())
+entities = (df[['Entities']])
+print(entities.head())
+# print(entities.to_string())
